37f1df02f6faeb958926ef76b43a3ef727c2f36c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-06 10:49:27 +0200

    set up searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function SearchBar() {
+  return (
+    <header>
+      <select id="togglesort" name="togglesort">
+        <option value="all">Sort by</option>
+        <option value="title-asc">Title A-Z</option>
+        <option value="title-desc">Title Z-A</option>
+        <option value="artist-asc">Artist A-Z</option>
+        <option value="artist-desc">Artist Z-A</option>
+        <option value="album-asc">Album A-Z</option>
+        <option value="album-desc">Album Z-A</option>
+        <option value="relDate-asc">Release date (ascending)</option>
+        <option value="relDate-desc">Release date (descending)</option>
+        <option value="price-asc">Price (ascending)</option>
+        <option value="price-desc">Price (descending)</option>
+      </select>
+      <select name="togglefilter" id="togglefilter">
+        <option value="all">Filter by</option>
+        <option value="artistName">Artist</option>
+        <option value="trackName">Title</option>
+        <option value="collectionName">Album</option>
+      </select>
+      <input type="text" id="filter" placeholder="Enter your filter string" />
+      <input type="text" id="searchfield" placeholder="Enter your search string" />
+      <button id="searchbutton">Search</button>
+    </header>
+  )
+}
