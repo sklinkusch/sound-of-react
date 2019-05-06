c90ef78d7861dfd446cd02acb1278c86a2fbf25f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-06 11:11:54 +0200

    initialized track list

---
+++ src/components/TrackList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class TrackList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Tracks</h1>
+        <p>NUM music tracks from iTunes</p>
+        <p>Here comes the track list.</p>
+      </div>
+    )
+  }
+}
