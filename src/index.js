d81e4ade7bcc3110f47f63d23832132681605154
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-06 13:27:29 +0200

    included fontawesome-icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
