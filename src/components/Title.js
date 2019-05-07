f6bb0543387722579ece789bb6aa984c66b16fc4
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-07 11:23:56 +0200

    outsourced title in its own component

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/Title.scss";
+
+export default function Title() {
+  return (
+    <h1>
+      My Tracks
+    </h1>
+  )
+}
--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import TrackListItem from './TrackListItem';
 import "../styles/TrackList.scss";
+import Title from './Title';
 
 export default function TrackList(props) {
   const numTracks = props.music.length
   return (
     <main>
-      <h1>My Tracks</h1>
+      <Title />
       <p className="trackNum">{numTracks} music tracks from iTunes</p>
       {props.music && numTracks > 0 ? props.music.map((track, index) => <TrackListItem music={track} key={index} playMusic={i => props.playMusic(i)} pauseMusic={() => props.pauseMusic()} />) : <div>No tracks have been found</div>}
     </main>
