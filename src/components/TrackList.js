cfd41f45d05265e46041f5bba4c32802ca8c918c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-07 10:41:31 +0200

    exported styles to TrackList.scss

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import TrackListItem from './TrackListItem';
+import "../styles/TrackList.scss";
 
 export default function TrackList(props) {
   const numTracks = props.music.length
   return (
     <main>
       <h1>My Tracks</h1>
-      <p>{numTracks} music tracks from iTunes</p>
+      <p className="trackNum">{numTracks} music tracks from iTunes</p>
       {props.music && numTracks > 0 ? props.music.map((track, index) => <TrackListItem music={track} key={index} playMusic={i => props.playMusic(i)} pauseMusic={() => props.pauseMusic()} />) : <div>No tracks have been found</div>}
     </main>
   )
