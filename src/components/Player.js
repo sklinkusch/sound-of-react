830ecd5859f97431efed0f3a231aca31577b41e5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-07 12:38:02 +0200

    removed autoplay

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Player extends Component {
   render() {
     return (
-      <audio loop src="" autoPlay ref={this.props.playerRef} />
+      <audio loop src="" ref={this.props.playerRef} />
     )
   }
 }
