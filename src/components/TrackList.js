109070879c34886e839e4b36437a761b5b98e931
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-07 09:00:01 +0200

    changed tracklist container to main

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -4,10 +4,10 @@ import TrackListItem from './TrackListItem';
 export default function TrackList(props) {
   const numTracks = props.music.length
   return (
-    <div>
+    <main>
       <h1>My Tracks</h1>
       <p>{numTracks} music tracks from iTunes</p>
       {props.music && numTracks > 0 ? props.music.map((track, index) => <TrackListItem music={track} key={index} playMusic={i => props.playMusic(i)} pauseMusic={() => props.pauseMusic()} />) : <div>No tracks have been found</div>}
-    </div>
+    </main>
   )
 }
