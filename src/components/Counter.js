19559028064fe675a5453edaeb45bb899de6a5a3
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-07 11:34:30 +0200

    added counter

---
+++ src/components/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Counter.scss";
+
+export default function Counter(props) {
+  const { count } = props;
+  return (
+    <React.Fragment>
+      {count > 1 && <p className="trackNum">{count} music tracks from iTunes </p>}
+      {count === 1 && <p className="trackNum">{count} music track from iTunes</p>}
+      {count === 0 && <p className="trackNum">No music tracks from iTunes</p>}
+    </React.Fragment>
+  )
+}
--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -2,13 +2,14 @@ import React from 'react'
 import TrackListItem from './TrackListItem';
 import "../styles/TrackList.scss";
 import Title from './Title';
+import Counter from './Counter';
 
 export default function TrackList(props) {
   const numTracks = props.music.length
   return (
     <main>
       <Title />
-      <p className="trackNum">{numTracks} music tracks from iTunes</p>
+      <Counter count={numTracks} />
       {props.music && numTracks > 0 ? props.music.map((track, index) => <TrackListItem music={track} key={index} playMusic={i => props.playMusic(i)} pauseMusic={() => props.pauseMusic()} />) : <div>No tracks have been found</div>}
     </main>
   )
