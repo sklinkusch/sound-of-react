c526040d0703cc43d74983c67ce866cc53f74aaf
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-04-22 12:40:53 +0200

    rename index.scss to index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.css';
-import './styles/index.scss';
+import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
